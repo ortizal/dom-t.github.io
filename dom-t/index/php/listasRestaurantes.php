4055cda0d23ba36806fe117d15ba62e46eb2e2f9
ortizal.github.io <alexis.ortiz81@outlook.com>
2020-05-14 20:36:09 -0500

    updated home page

--- dom-t/index/php/listasRestaurantes.php
+++ dom-t/index/php/listasRestaurantes.php
@@ -1,12 +1,22 @@
-<?php 
+<table>
+<tr>
+    <th>NOMBRE</th>
+</tr>
+    <?php 
     require_once '../../conexion/php/conexion.php';
     function get_listasRestaurante(){
         $conn = getConn();   
         $query = 'SELECT * FROM restaurante';
         $resultado = $conn->query($query);
         while ( $row = $resultado->fetch_array(MYSQLI_ASSOC)){
-           echo "<p>$row[nombre]</p></br>";
+            $restaurante = "<p>$row[nombre]</p></br>";
+            ?>
+            <tr>
+                <td><?php echo $restaurante?></td>
+            </tr>
+            <?php
         }    
     }
     echo get_listasRestaurante();
-?>
+    ?>
+</table>
