f5a6cbc5394d6cfa0ed3eb8493f32bb8f1f54310
ortizal.github.io <alexis.ortiz81@outlook.com>
2020-05-14 19:37:58 -0500

    updated home page

--- dom-t/conexion/php/conexion.php
+++ dom-t/conexion/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
     function getConn(){
-        $conexion=mysqli_connect("localhost","root","","dom-t");
+        $conexion=mysqli_connect("sql10.freesqldatabase.com","sql10340705","4UWmtv6RC6","sql10340705");
         $conexion->set_charset ('utf8');
         return $conexion;
     }
